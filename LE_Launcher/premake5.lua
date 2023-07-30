4489402f1b4ff5dfdaa5b14f9c2500b1443098e0
Kátai Szilárd <szilatika@gmail.com>
2023-07-30 14:42:01 +0200

    Project launcher WIP

---
+++ LE_Launcher/premake5.lua
@@ -0,0 +1,57 @@
+project "LE_Launcher"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir)
+	objdir ("%{wks.location}/bin-int/" .. outputdir)
+	debugdir ("%{wks.location}/bin/" .. outputdir)
+
+	dependson
+	{
+		"LearningEngine"
+	}
+
+	files
+	{
+		"**.h",
+		"**.cpp"
+	}
+
+	includedirs
+	{
+		"%{wks.location}/LearningEngine",
+		"%{wks.location}/LearningEngine/src",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.ENTT}"
+	}
+	
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	links
+	{
+		"LearningEngine"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "LE_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "LE_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "LE_DIST"
+		runtime "Release"
+		optimize "on"
