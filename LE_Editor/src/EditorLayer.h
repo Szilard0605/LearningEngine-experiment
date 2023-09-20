de2f671483589980c91068bab6cfaf0b8c928922
Szilard0605 <szilatika@gmail.com>
2023-09-20 23:49:02 +0200

    Changed the default working directories to the projects directory

---
+++ LE_Editor/src/EditorLayer.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "LearningEngine.h"
+
+#include "Panels/EntityListPanel.h"
+#include "Panels/ContentBrowser.h"
+#include "EditorRuntime.h"
+
+class EditorLayer : public Layer
+{
+public:
+	EditorLayer();
+	virtual ~EditorLayer() = default;
+
+	virtual void OnAttach() override;
+	virtual void OnDetach() override;
+	virtual void OnEvent(Event& event) override;
+	virtual void OnImGuiRender() override;
+	
+	//Events
+	bool OnKeyChange(KeyEvent& keyevent);
+	bool OnMouseButtonChange(MouseButtonEvent& event);
+	bool OnMouseMove(MouseMoveEvent& event);
+	bool OnMouseScrolled(MouseScrolledEvent& event);
+
+	void OnUpdate(Timestep timestep) override;
+
+	void UpdateGizmos();
+
+	static glm::vec2 GetMainViewportSize() { return s_MainViewportSize; };
+
+private:
+
+	Scene* m_Scene;
+	Framebuffer* m_Framebuffer;
+	PerspectiveCamera* m_EditorCamera;
+	bool m_ViewportActive = false;
+	bool m_ViewportHovered = false;
+
+	static glm::vec2 s_MainViewportSize;
+	glm::vec2 m_ViewportBounds[2];
+
+	glm::vec2 LastMousePos = {-1, -1};
+
+	entt::entity m_HoveredEntity;
+
+	int m_GizmoType = -1;
+
+	// Panels
+	EntityListPanel m_EntitiesPanel;
+	ContentBrowser m_ContentBrowser;
+
+	Texture2D* m_TexPlayButton;
+	bool m_PressedPlay = false;
+
+	Texture2D* m_TranslateIcon;
+	Texture2D* m_RotateIcon;
+	Texture2D* m_ScaleIcon;
+	bool m_OperationIconHovered = false;
+
+
+	EditorRuntime m_Runtime;
+};
+
---
+++ LE_Editor/src/Panels/EntityListPanel.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "entt.hpp"
+#include "Graphics/Scene/Entity.h"
+
+class Scene;
+
+class EntityListPanel
+{
+public:
+	EntityListPanel() = default;
+	EntityListPanel(Scene* scene);
+
+	void Render();
+	void SetScene(Scene* scene);
+
+	entt::entity GetSelectedEntity() { return m_SelectedEntity; }
+	void SetSelectedEntity(entt::entity entity) { m_SelectedEntity = entity; }
+private:
+
+	void DisplayHierarchy(Entity entity);
+
+	Scene* m_Scene = nullptr;
+
+	bool m_DeleteEntity = false;
+	bool m_Enable = true;
+
+	entt::entity m_SelectedEntity = entt::null;
+};
+
